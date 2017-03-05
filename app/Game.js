3da6afa75a1d96cd6aed7e4491c14bfc74a0ede8
Kevin Lacker <lacker@gmail.com>
2017-03-05 10:14:25 -0800

    some Game stuff

---
+++ app/Game.js
@@ -0,0 +1,10 @@
+export default class Game {
+  // player1 and player2 are just string ids for the players
+  // player1 will go first
+  constructor(player1, player2) {
+    this.playerIds = [player1, player2];
+    this.turn = player1
+
+    // TODO: make hands and fields
+  }
+}
